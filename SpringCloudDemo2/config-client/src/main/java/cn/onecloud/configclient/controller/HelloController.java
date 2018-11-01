e2ee04f59ce0b5346bc818e3972269cb35920636
chengjw <1184813875@qq.com>
2018-11-01 22:34:29 +0800

    SpringCloudDemo2

--- SpringCloudDemo2/config-client/src/main/java/cn/onecloud/configclient/controller/HelloController.java
+++ SpringCloudDemo2/config-client/src/main/java/cn/onecloud/configclient/controller/HelloController.java
@@ -1,10 +1,24 @@
 package cn.onecloud.configclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @ClassName: HelloController
  * @Description:
  * @Author: Chengjw
  * @Date: 2018/11/1 22:30
  */
+
+@RestController
 public class HelloController {
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return foo;
+    }
 }
