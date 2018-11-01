0206a7c5278a53f0acea5a148da2a8984a2465a0
chengjw <1184813875@qq.com>
2018-11-01 22:33:19 +0800

    SpringCloudDemo2

---
+++ SpringCloudDemo2/config-client/src/main/java/cn/onecloud/configclient/controller/HelloController.java
@@ -0,0 +1,10 @@
+package cn.onecloud.configclient.controller;
+
+/**
+ * @ClassName: HelloController
+ * @Description:
+ * @Author: Chengjw
+ * @Date: 2018/11/1 22:30
+ */
+public class HelloController {
+}
